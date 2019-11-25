af7b308ea0242a562b9e6fe3b7213e9bc6a73fce
GEORGE JINU <george.jinu@outlook.com>
2019-11-24 20:00:38 -0500

    Added Performace improvement

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './Backdrop.css'
 
 const backDrop = (props) => (
-    props.show ? <div onClick={props.handleBackDrop} className={classes.Backdrop}></div> : null
+    props.show ? <div onClick={props.clicked} className={classes.Backdrop}></div> : null
 )
 
 export default backDrop;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.css'
 import Aux from '../../../hoc/Auxiliary'
 import BackDrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-    <Aux>
-        <BackDrop show={props.show} handleBackDrop={props.handleBackDrop} />
-        <div className={classes.Modal}
-            style={{
-                transform: props.show ? 'transilateY(0)' : 'transilateY(-100vh)',
-                opacity: props.show ? 1 : 0,
-                display: props.show ? 'inline' : 'none'
-                
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
+    componentWillUpdate(){
+        console.log('[Modal] will update')
+    }    
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    render() {
+        return (
+            <Aux>
+                <BackDrop show={this.props.show} clicked={this.props.handleBackDrop} />
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'transilateY(0)' : 'transilateY(-100vh)',
+                        opacity: this.props.show ? 1 : 0,
+                        display: this.props.show ? 'inline' : 'none'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
